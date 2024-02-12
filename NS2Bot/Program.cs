03edb0e73cf3f983bb5210bbb30767506de93a03
fkn_goose <70691415+zezkek@users.noreply.github.com>
2024-02-12 20:41:21 +0300

    init

---
+++ NS2Bot/Program.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace NS2Bot
+{
+    public class Program
+    {
+        private static DiscordSocketClient _client;
+        public async static void Main(string[] args)
+        {
+            var token = File.ReadAllText("../../../token.txt");
+
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private static Task Log(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
