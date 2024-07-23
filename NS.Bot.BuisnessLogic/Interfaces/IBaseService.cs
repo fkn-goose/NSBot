1a828c3edf8d5139a724b391964346ababf1a08e
fkn_goose <70691415+zezkek@users.noreply.github.com>
2024-07-23 22:38:36 +0300

    clear cache

--- NS.Bot.BuisnessLogic/Interfaces/IBaseService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NS.Bot.BuisnessLogic.Interfaces
-{
-    public interface IBaseService<T>
-    {
-        void Update(T entity);
-        void Create(T entity);
-    }
-}
--- NS.Bot.Shared/Entities/BaseEntity.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NS.Bot.Shared.Entities
-{
-    public class BaseEntity
-    {
-        public long Id { get; set; }
-    }
-}
